d6953390eb895484500c915afe5336fade03584a
ArtA <pthung7102002@gmail.com>
2022-08-25 23:55:41 +0700

    1.1

--- src/commands/tools/prefix.js
+++ src/commands/tools/prefix.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, PermissionsBitField } = require("discord.js");
+const { SlashCommandBuilder, PermissionFlagsBits } = require("discord.js");
 const content = (str, private = true) => ({
   content: str,
   ephemeral: private,
@@ -12,22 +12,10 @@ module.exports = {
     .setName("prefix")
     .setDescription("Shows prefix commands or Set the prefix of the bot.")
     .addStringOption((option) =>
-      option.setName("change").setDescription("Set new prefix for 3wE.")
+      option.setName("change").setDescription("Set new prefix for 3wE")
     )
-    .addStringOption((option) =>
-      option
-        .setName("activate")
-        .setDescription("Turn on or off prefix commands.")
-        .setChoices(
-          {
-            name: "on",
-            value: "on",
-          },
-          {
-            name: "off",
-            value: "off",
-          }
-        )
+    .addBooleanOption((option) =>
+      option.setName("activate").setDescription("Change activation to")
     ),
   async execute(interaction, client) {
     // Get current prefix from database
@@ -35,15 +23,11 @@ module.exports = {
 
     // Get options
     const newPrefix = interaction.options.getString("change");
-    const changeActivate = interaction.options.getString("activate");
+    const changeActivate = interaction.options.getBoolean("activate");
 
     // Check permissions
     if (newPrefix !== null || changeActivate !== null) {
-      if (
-        !interaction.memberPermissions.has(
-          PermissionsBitField.Flags.ManageGuild
-        )
-      ) {
+      if (!interaction.memberPermissions.has(PermissionFlagsBits.ManageGuild)) {
         return await interaction.reply(
           content("You don't have permission to do that!")
         );
@@ -51,56 +35,45 @@ module.exports = {
     }
 
     // Execute
-    // If set prefix
-    if (newPrefix !== null) {
-      if (guild.prefix.current === newPrefix) {
+    // If change activate
+    if (changeActivate !== null) {
+      if (guild.prefix.activation === changeActivate) {
         return await interaction.reply(
-          content(`Prefix is already **\`${newPrefix}\`**!`)
+          content(`Prefix is already **\`${changeActivate ? "On" : "Off"}\`**!`)
         );
       }
+
+      await guild
+        .updateOne({ $set: { "prefix.activation": changeActivate } })
+        .catch((e) => console.error(e.message));
+
+      return await interaction.reply(
+        `Prefix has been set to **\`${changeActivate ? "On" : "Off"}\`**`
+      );
+    }
+
+    // If deactivate
+    if (guild.prefix.activation === false)
+      return await interaction.reply(content("Server prefix is **`Off`**"));
+
+    // If set prefix
+    if (newPrefix) {
+      if (guild.prefix.current === newPrefix) {
+        return await interaction.reply(
+          content(`Prefix is already **\`${newPrefix}\`**`)
+        );
+      }
+
       await guild
         .updateOne({ $set: { "prefix.current": newPrefix } })
         .catch((e) => console.error(e.message));
-      // await guild.save().catch((e) => console.error(e.message));
+
       return await interaction.reply(
         `Prefix has been change to **\`${newPrefix}\`**`
       );
     }
 
-    // If change activate
-    if (changeActivate !== null) {
-      switch (changeActivate) {
-        case "on":
-          if (guild.prefix.activation === true) {
-            return await interaction.reply(
-              content("Prefix is already **`on`**!")
-            );
-          }
-
-          guild.prefix.activation = true;
-          await guild.save().catch((e) => console.error(e.message));
-          return await interaction.reply("Prefix has been set to **`on`**");
-
-        case "off":
-          if (guild.prefix.activation === false) {
-            return await interaction.reply(
-              content("Prefix is already **`off`**!")
-            );
-          }
-
-          guild.prefix.activation = false;
-          await guild.save().catch((e) => console.error(e.message));
-          return await interaction.reply("Prefix has been set to **`off`**");
-      }
-    }
-
-    // If deactivate
-    if (guild.prefix.activation === false) {
-      return await interaction.reply(content("Server prefix is **`off`**."));
-    }
-
     // If none of those
-
     return await interaction.reply(
       `Current server prefix: **\`${
         guild.prefix.current
---
+++ src/events/client/messageDeleteBulk.js
@@ -0,0 +1,18 @@
+const Guild = require("../../schemas/guild");
+
+module.exports = {
+  name: "messageDeleteBulk",
+  async execute(messages, channel, client) {
+    const guild = await Guild.findOne({ id: channel.guildId });
+    const historyChannelId = guild.history;
+
+    if (!historyChannelId) return;
+    if (historyChannelId === channel.id) return;
+
+    const historyChannel = client.channels.cache.find(
+      (channel) => channel.id == historyChannelId
+    );
+
+    messages.forEach(async (message) => await historyChannel.send(message));
+  },
+};
