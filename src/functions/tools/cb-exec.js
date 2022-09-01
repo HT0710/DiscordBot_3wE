503910ea1013e253d5e5c10d7b14e8077a3fb66b
ArtA <pthung7102002@gmail.com>
2022-09-01 07:32:02 +0700

    1.4

---
+++ src/functions/tools/cb-exec.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const path = require("path");
+const proc = require("child_process");
+
+module.exports = (data) => {
+  const content = `import clipboard from "clipboardy"; clipboard.writeSync("${data}");`;
+
+  fs.writeFileSync("./src/modules/copy-to-clipboard.js", content);
+  const exec = path.join(__dirname, "../../modules/copy-to-clipboard.js");
+
+  proc.exec(`node ${exec}`);
+};
