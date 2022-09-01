bd5ab51ed4ccf919190f7d2e5040b2e6593f2853
ArtA <pthung7102002@gmail.com>
2022-09-01 21:42:28 +0700

    1.5

--- src/commands/contexts/message/save.js
+++ src/commands/contexts/message/save.js
@@ -10,29 +10,31 @@ module.exports = {
     .setName("Save this")
     .setType(ApplicationCommandType.Message),
   async execute(interaction, client) {
+    await interaction.deferReply({ ephemeral: true });
+
     let message = await interaction.targetMessage.fetch();
     message.nonce = Math.random().toString().slice(2);
 
-    try {
-      await interaction.member.send(message);
+    await interaction.member.send(message).catch((e) => {
+      message.nonce = Math.random().toString().slice(2);
 
-      await interaction.reply({
-        embeds: [
-          new EmbedBuilder().setColor(Colors.Green).setTitle("```✅ Saved!```"),
-        ],
-        ephemeral: true,
+      interaction.member.send(message).catch((e) => {
+        console.error(e.message);
+
+        const errorEmbed = new EmbedBuilder()
+          .setColor(Colors.Red)
+          .setTitle(
+            "```This message could not be sent to you! Check your privacy settings.```"
+          );
+
+        return interaction.editReply({ embeds: [errorEmbed] });
       });
-    } catch (e) {
-      interaction.reply({
-        embeds: [
-          new EmbedBuilder()
-            .setColor(Colors.Red)
-            .setTitle(
-              "```This message could not be sent to you! Check your privacy settings.```"
-            ),
-        ],
-        ephemeral: true,
-      });
-    }
+    });
+
+    const doneEmbed = new EmbedBuilder()
+      .setColor(Colors.Green)
+      .setTitle("```✅ Saved!```");
+
+    await interaction.editReply({ embeds: [doneEmbed] });
   },
 };
